b6b666de647aab173af25a4122bf75953c5a616c
DESKTOP-RI8P93N\Gyu-Ha <27statsnerd@gmail.com>
2022-11-22 23:17:27 +0900

    - 총알 풀링 구현

--- Assets/Scripts/Bullet.cs
+++ Assets/Scripts/Bullet.cs
@@ -4,12 +4,22 @@ using UnityEngine;
 
 public class Bullet : MonoBehaviour
 {
+    Transform Parent;   
+
+    public void SetMyParent(Transform parent)
+    {
+        Parent = parent;
+    }
 
     void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.transform.CompareTag("Bullet Border"))
         {
-            Destroy(gameObject);
+            Rigidbody2D rigid = GetComponent<Rigidbody2D>();
+            bool isLeft = rigid.velocity.y > 0 ? false : true;
+
+            gameObject.SetActive(false);
+            Parent.GetComponent<ShootBullet>().PoolIn(gameObject,isLeft);
         }
     }
 }
--- Assets/Scripts/ShootBullet.cs
+++ Assets/Scripts/ShootBullet.cs
@@ -7,10 +7,14 @@ public class ShootBullet : MonoBehaviour
     public float maxTime;         // 재장전 필요 시간
     public float curTime;         // 마지막 발사로부터 소요한 시간
     public float bulletPower;     // 총알 발사 파워
+    public bool isUp;
 
-    public GameObject[] BulletPrefab;
+    [SerializeField] GameObject UpBulletPrefab;
+    [SerializeField] GameObject LeftBulletPrefab;
+    List<GameObject> UpBulletPrefabList = new List<GameObject>();
+    List<GameObject> LeftBulletPrefabList = new List<GameObject>();
 
-    void Start()
+    void Awake()
     {
         
     }
@@ -22,11 +26,81 @@ public class ShootBullet : MonoBehaviour
 
         if(curTime >= maxTime)
         {
-            GameObject bullet = Instantiate(BulletPrefab[0], transform.position, transform.rotation);
-            Rigidbody2D rigid = bullet.GetComponent<Rigidbody2D>();
-            rigid.AddForce(Vector2.up * bulletPower, ForceMode2D.Impulse);
+            Create();
+            Shoot(PoolOut());
 
             curTime = 0;
         }
+    }
+
+    void Create()
+    {
+        if (isUp && UpBulletPrefabList.Count == 0)
+        {
+            GameObject bullet = Instantiate(UpBulletPrefab, transform.position, transform.rotation);
+            bullet.GetComponent<Bullet>().SetMyParent(transform);
+            UpBulletPrefabList.Add(bullet);
+            bullet.SetActive(false);
+        }
+        else if (!isUp && LeftBulletPrefabList.Count == 0)
+        {
+            GameObject bullet = Instantiate(LeftBulletPrefab, transform.position, transform.rotation);
+            bullet.GetComponent<Bullet>().SetMyParent(transform);
+            LeftBulletPrefabList.Add(bullet);
+            bullet.SetActive(false);
+        }
+    }
+
+    GameObject PoolOut()           // 마지막에 만들어진 프리팹의 GameObject 자료형을 반환
+    {
+        if(isUp)
+        {
+            int LastIndex = UpBulletPrefabList.Count - 1;
+            GameObject bullet = UpBulletPrefabList[LastIndex];
+            UpBulletPrefabList.Remove(bullet);
+            bullet.SetActive(true);
+
+            return bullet;
+        }
+        else
+        {
+            int LastIndex = LeftBulletPrefabList.Count - 1;
+            GameObject bullet = LeftBulletPrefabList[LastIndex];
+            LeftBulletPrefabList.Remove(bullet);
+            bullet.SetActive(true);
+
+            return bullet;
+        }
+    }
+
+    void Shoot(GameObject bullet)
+    {
+        Rigidbody2D rigid = bullet.GetComponent<Rigidbody2D>();
+
+        if (isUp)                // 위로 쏘는 오브젝트면 총알을 위로 발사
+        {
+            rigid.AddForce(Vector2.up * bulletPower, ForceMode2D.Impulse);
+        }
+        else
+        {
+            rigid.AddForce(Vector2.left * bulletPower, ForceMode2D.Impulse);
+            bullet.transform.localEulerAngles = new Vector3(0, 0, 90);
+        }
+    }
+
+    public void PoolIn(GameObject bullet, bool isLeft)
+    {
+        if(isLeft)
+        {
+            LeftBulletPrefabList.Add(bullet);
+            bullet.SetActive(false);
+        }
+        else
+        {
+            UpBulletPrefabList.Add(bullet);
+            bullet.SetActive(false);
+        }
+
+        bullet.transform.position = transform.position;           // 총알의 위치를 총알 쏘는 위치로 다시 초기화
     }
 }
