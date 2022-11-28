dba78c58567b0efad0153a40f7244aa0ca277cae
DESKTOP-RI8P93N\Gyu-Ha <27statsnerd@gmail.com>
2022-11-28 23:47:52 +0900

    - 게임씬 리로드 할 때 오브젝트가 빠지는 현상 수정

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -58,6 +58,7 @@ public class GameManager : MonoBehaviour
     public Sprite PauseButton;
 
     bool isPaused;
+    bool onClickPauseButton;
 
     void Awake()
     {
@@ -71,7 +72,9 @@ public class GameManager : MonoBehaviour
         }
 
         DontDestroyOnLoad(gameObject);
-        gameScene = SceneManager.GetActiveScene().buildIndex;     // ���� �Ŵ����� �ִ� �� ��ȣ(�÷����� ��)�� ����
+        gameScene = SceneManager.GetActiveScene().buildIndex;     // 게임 매니저 스크립트를 불러오는 씬 번호를 gameScene으로 저장
+
+        SceneManager.sceneLoaded += OnSceneLoaded;
     }
 
     void Start()
@@ -84,11 +87,14 @@ public class GameManager : MonoBehaviour
         if (Input.GetKeyDown(KeyCode.R))
         {
             SceneManager.LoadScene(gameScene);
-            InitVariables();
+            LapsedTime = 0;
         }
 
         SetTimeUI();
-        SetHealthUI();
+        if(player)
+        {
+            SetHealthUI();
+        }
     }
 
     void SetTimeUI()
@@ -106,6 +112,14 @@ public class GameManager : MonoBehaviour
     {
         int playerHP = FindObjectOfType<Player>().GetComponent<Player>().Health;
         healthUI.text = "HP: " + playerHP.ToString();
+    }
+
+    void OnSceneLoaded(Scene scene, LoadSceneMode mode)
+    {
+        if(SceneManager.GetActiveScene().buildIndex == gameScene)       // 현재 씬 번호와 게임 씬 번호를 비교
+        {
+            InitVariables();
+        }
     }
 
     void InitVariables()
@@ -130,20 +144,47 @@ public class GameManager : MonoBehaviour
             healthUI = GameObject.Find("HP UI").GetComponent<TextMeshProUGUI>();
         }
 
+        if (!timeUI)
+        {
+            timeUI = GameObject.Find("Time UI").GetComponent<TextMeshProUGUI>();
+        }
+
+        if (!clearText)
+        {
+            clearText = GameObject.Find("Canvas").transform.Find("Clear_Text").gameObject.GetComponent<TextMeshProUGUI>();
+        }
+
         if (!RetryButton)
         {
-            RetryButton = GameObject.Find("Retry Button");
+            RetryButton = GameObject.Find("Canvas").transform.Find("Retry Button").gameObject;
+            Button retryButton = RetryButton.GetComponent<Button>();
+            retryButton.onClick.AddListener(OnClickRetryButton);
         }
 
         if (!QuitButton)
         {
-            QuitButton = GameObject.Find("Quit Button");
+            QuitButton = GameObject.Find("Canvas").transform.Find("Quit Button").gameObject;
+            Button quitButton = QuitButton.GetComponent<Button>();
+            quitButton.onClick.AddListener(OnClickQuitButton);
+        }
+
+        Button button = GameObject.Find("Pause Button").GetComponent<Button>();
+        if(button.onClick.GetPersistentEventCount()<=1)
+        {
+            button.onClick.AddListener(OnClickPauseButton);
         }
     }
 
 
     public void OnClickPauseButton()
     {
+        //if(onClickPauseButton)
+        //{
+        //    return;
+        //}
+
+        //onClickPauseButton = true;
+
         Image buttonImg = GameObject.Find("Pause Button").GetComponent<Image>();
         isPaused = !isPaused;
         
@@ -153,23 +194,35 @@ public class GameManager : MonoBehaviour
             Time.timeScale = 0;
             buttonImg.sprite = PlayButton;
             Camera.main.GetComponent<AudioSource>().Pause();
+
+            GameObject.Find("Canvas").transform.Find("Retry Button").gameObject.SetActive(true);
+            GameObject.Find("Canvas").transform.Find("Quit Button").gameObject.SetActive(true);
         }
         else
         {
             Time.timeScale = 1;
             buttonImg.sprite = PauseButton;
             Camera.main.GetComponent<AudioSource>().Play();
+
+            GameObject.Find("Canvas").transform.Find("Retry Button").gameObject.SetActive(false);
+            GameObject.Find("Canvas").transform.Find("Quit Button").gameObject.SetActive(false);
         }
     }
 
     public void OnClickRetryButton()
     {
+        isPaused = !isPaused;
         SceneManager.LoadScene(gameScene);
+        Time.timeScale = 1;
+        LapsedTime = 0;
     }
 
     public void OnClickQuitButton()
     {
+        isPaused = !isPaused;
         SceneManager.LoadScene(gameScene - 1);
+        Time.timeScale = 1;
+        LapsedTime = 0;
     }
 
     public void SetSavePoint()
